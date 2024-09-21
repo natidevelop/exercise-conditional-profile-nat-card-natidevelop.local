bc1a9ce6d133d2f8aad268983da1a19a0d1b0ee1
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-09-21 01:19:21 +0000

    trabajo en equipo

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,17 @@
 import "../style/index.css";
-
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
-        
         twitter: null, // social media usernames
         github: null,
         linkedin: null,
         instagram: null,
-
         name: null,
         lastName: null,
         role: null,
@@ -28,24 +25,44 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Lucy" : variables.name}
+          ${variables.lastName == null ? "Boilett" : variables.lastName}
+          </h1>
+          <h2>${variables.role == null ? "Web Developer" : variables.role}</h2>
+          <h3>${variables.city == null ? "Miami" : variables.city}, ${
+    variables.country == null ? "USA" : variables.country
+  }
+    </h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href=${
+              variables.twitter == null
+                ? "https://twitter.com/4geeksacademy"
+                : "https://twitter.com/" + variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github == null
+                ? "https://github.com/4geeksacademy"
+                : "https://github.com/" + variables.github
+            }>
+                <i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin == null
+                ? "https://linkedin.com/school/4geeksacademy"
+                : "https://linkedin.com/school/" + variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram == null
+                ? "https://instagram.com/4geeksacademy"
+                : "https://instagram.com/" + variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -58,7 +75,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
     github: null,
@@ -71,7 +88,6 @@ window.onload = function() {
     city: null
   };
   render(window.variables); // render the card for the first time
-
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
       // <- add a listener to every input
